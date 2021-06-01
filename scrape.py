538c288ca412735de4dff17bfec87f1e012cf9cb
averybostick <avery@bouki.com>
2021-06-01 11:13:09 -0500

    each piece of data put in a list, all lists in a dictionary, used pandas to convert to dataframe and exported to csv

--- scrape.py
+++ scrape.py
@@ -1,12 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
-from bs4 import SoupStrainer
-import urllib.request
-import re
-import asyncio
-import os
-from pyppeteer import launch
 
+from bs4 import BeautifulSoup
+import asyncio
+
+from pyppeteer import launch
+import pandas as pd
 
 name = []
 price = []
@@ -28,10 +25,41 @@ async def main():
         
     page_content = await page.content() 
     soup = BeautifulSoup(page_content, 'html.parser')
+    i=0
     for trs in soup.select('tr'):
-        print (trs.text)
+        if i==0:
+            i += 1
+            continue
 
-    await browser.close()
+        p = trs.select('p')
+        a = trs.select('a')
+        span = trs.select('span')
+
+        name.append(p[1].text)
+        price.append(a[1].text)  
+        daily_percent.append(span[2].text)   
+        weekly_percent.append(span[4].text)  
+
+        long = p[3].text.split('$')
+        market_cap.append('$'+long[2])   
+
+        volume.append(p[4].text)      
+        circulating_supply.append(p[6].text)
+    print(name)  
+
+    dict = {'name':name,
+            'price': price,
+            'daily_percent': daily_percent,
+            'weekly_percent': weekly_percent,
+            'market_cap':market_cap,
+            'volume':volume,
+            'circulating_supply':circulating_supply
+    }
+
+    df = pd.DataFrame(dict)
+    df.to_csv('crypto.csv')
+
+    await browser.close() 
     
 
 asyncio.get_event_loop().run_until_complete(main())
