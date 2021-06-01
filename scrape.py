d0f137259b10d1f7698b00cd60b4dd152338ea0d
averybostick <avery@bouki.com>
2021-06-01 10:28:14 -0500

    got the scraping to work. using pyppeteer to run async calls and use a virtual keyboard to scroll down to load all data

--- scrape.py
+++ scrape.py
@@ -1,12 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import urllib.request
+import re
+import asyncio
+import os
+from pyppeteer import launch
 
-res = requests.get('https://coinmarketcap.com')
-soup = BeautifulSoup(res.content, 'html.parser')
 
-txt = res.text
-status = res.status_code
-title = soup.title.text
+name = []
+price = []
+daily_percent = []
+weekly_percent = []
+market_cap = []
+volume = []
+circulating_supply = []
 
-print(status, title)
+async def main():
 
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+    page_path = "https://coinmarketcap.com"
+    await page.goto(page_path)
+
+    for i in range(200):
+        await page.keyboard.press('ArrowDown')
+        
+    page_content = await page.content() 
+    soup = BeautifulSoup(page_content, 'html.parser')
+    for trs in soup.select('tr'):
+        print (trs.text)
+
+    await browser.close()
+    
+
+asyncio.get_event_loop().run_until_complete(main())
